d5226688d0f91ae50f596441e9c9092ae614024a
EBRIMA SONKO <82295266+ibra188@users.noreply.github.com>
2024-10-30 23:09:00 +0100

    day one
    day one with it excises completed.

---
+++ dayone1.py
@@ -0,0 +1,98 @@
+
+# coment in python are use for redability for developers and feture references
+"""
+this is multiline comment for better descriptions
+"""
+
+# Data types in pythons
+# 1 NUMBERS
+   #Intergers: -1, 2, 0
+   #float: -1.5, 2.3, 0.0
+# 2 STRING is a collection of characters in single or double quote
+     #Example 'ibra sonko'
+     # "eletricista"
+# BOOLEAN is a data type of true or false
+   #True False
+
+#LIST is an ordered collection wich allowed to store difrent data types
+['ibra sonko'] #string data type in list
+[-1,2,3.3] #intergers data type in lis
+[False, True] #boolean data type in list
+
+#DICTIONARY
+ #A Python dictionary object is an unordered collection of data in a key value pair format.
+{
+    'name': 'ibra',
+    'last_name': 'sonko',
+    'age': 24,
+    'ocopetion': 'eletricista',
+    'hobby': 'programming',
+    'car_model': 'fiat punto',
+    'skills': ['eletrician', 'python', 'mobile repaire']
+}
+#TUPLE 
+   #A tuple is an ordered collection of different data types like list but tuples can not be modified once they are created. They are immutable.
+('ibra', 'eletricista')
+(1, 2.2, -2.4)
+
+#SETS
+  #A set is a collection of data types similar to list and tuple. Unlike list and tuple, set is not an ordered collection of items. Like in Mathematics, set in Python stores only unique items.
+{200, 2.50}
+{500, -500}
+
+#Exercises - Day 1
+#1. Check the python version you are using
+#2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+#addition(+)
+print(type(2.3+1))
+#subtraction(-)
+print(type(3-2))
+#multiplication(*)
+print(type(2*2))
+#modulus(%)
+print(type(2%4))
+#division(/)
+print(type(2/6))
+#exponential(**)
+print(type(2**3))
+#floor division operator(//)
+print(type(2 // 8))
+
+
+#3. Write strings on the python interactive shell. The strings are the following:
+#Your name
+print('ibra')
+#Your family name
+print('buba', 'amie', 'jj')
+#Your country
+print('Gambia')
+#I am enjoying 30 days of python
+print('I am enjoying 30 days of python')
+
+#4. Check the data types of the following data:
+#10
+print(type(10))
+#9.8
+print(type(9.8))
+#3.14
+print(type(3.14))
+#4 - 4j
+print(type(4 - 4j))
+#['Asabeneh', 'Python', 'Finland']
+print(type(['ibra', 'Python', 'Gambia']))
+#Your name
+print(type('ibra'))
+#Your family name
+print(type('my family'))
+#Your country
+print(type('Gambia'))
+
+#Exercise: Level 2
+
+#Create a folder named day_1 inside 30DaysOfPython folder. Inside day_1 folder, 
+# create a python file helloworld.py and repeat questions 1, 2, 3 and 4.
+# Remember to use print() when you are working on a python file. 
+# Navigate to the directory where you have saved your file, and run it.
+"""
+I just did it without the intergrated shell
+"""
